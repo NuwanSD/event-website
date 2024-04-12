c3875e0c0a0b07c2a9fa25ade5cc24c89ac20996
Sagara Nuwan <sagaradnuwan@gmail.com>
2024-04-12 20:25:15 +0530

    updated

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "evenly",
+      dbName: "evently",
       bufferCommands: false,
     });
 
